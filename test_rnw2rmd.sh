13da4d22451b957a086c0493259001e0fe850949
lwaldron <lwaldron.research@gmail.com>
2022-06-01 12:33:39 -0400

    add rnw2rmd.pl test

---
+++ test_rnw2rmd.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/nxskok/stad29-notes/712d5e99be7add5fac7407b0a189541c5d63fdb4/rnw2rmd.pl
+chmod 755 rnw2rmd.pl
+./rnw2rmd.pl BiocParallel_BatchtoolsParam.Rnw > BiocParallel_BatchtoolsParam.Rmd
+./rnw2rmd.pl Errors_Logs_And_Debugging.Rnw > Errors_Logs_And_Debugging.Rmd
+./rnw2rmd.pl Introduction_To_BiocParallel.Rnw > Introduction_To_BiocParallel.Rmd
+./rnw2rmd.pl Random_Numbers.Rnw > Random_Numbers.Rmd
